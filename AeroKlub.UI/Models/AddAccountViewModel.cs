31e80bbb9bc1554be9eedb41dff8c1271d00ac31
Michał Kochanowicz <m.kochanowicz@op.pl>
2018-07-15 18:05:55 +0200

    Ref 0.2b

---
+++ AeroKlub.UI/Models/AddAccountViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AeroKlub.UI.Models
+{
+    public class AddAccountViewModel
+    {
+        [Required(ErrorMessage = "Proszę podać nazwę użytkownika!")]
+        public string Name { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string SecondName { get; set; }
+
+        public string paswd { get; set; }
+
+        public string secondpaswd { get; set; }
+
+        public string email { get; set; }
+
+    }
+}
